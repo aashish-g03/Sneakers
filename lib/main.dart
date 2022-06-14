807920a3cabb4bb5f27578f5558f90ec8925ec45
Anshul-101 <anshulbarleja0@gmail.com>
2022-06-14 23:10:01 +0530

    h

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:we_can_do_it/authentication/auth.dart';
 import 'package:we_can_do_it/shop/view/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:we_can_do_it/shop/view/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: AuthenticationPage(),
+        home: Profile_Page(),
       ),
     );
   }
